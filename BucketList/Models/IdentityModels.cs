e8497d88f822f2b0df4aecf42e865414f654d7ff
mskendrae <mskendrae@gmail.com>
2016-11-09 11:16:43 -0500

    add userproperties

--- BucketList/Models/IdentityModels.cs
+++ BucketList/Models/IdentityModels.cs
@@ -11,6 +11,11 @@ namespace BucketList.Models
 
     public class ApplicationUser : IdentityUser
     {
+        public override string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Bio { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
