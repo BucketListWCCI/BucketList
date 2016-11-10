e34f864cc8a316dc715e4450a48710cf17b25659
mskendrae <mskendrae@gmail.com>
2016-11-10 13:56:43 -0500

    allmodels-controllers

---
+++ BucketList/Models/entertainment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace BucketList.Models
+{
+    public class entertainment
+    {
+        [Key]
+        public int EntertainmentId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string EntertainmentType { get; set; }
+        public string Link { get; set; }
+        public string Location { get; set; }
+        public string EntertainmentIsComplete { get; set; }
+
+        public virtual ApplicationUser User { get; set; }   //This SHOULD be the fk
+    }
+}
